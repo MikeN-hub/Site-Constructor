b55c3c3756d843a6e7f8d3270e96206329791ef9
Mike Navrotsky <mikes.navr@gmail.com>
2021-03-07 16:24:56 +0200

    first stage

---
+++ index.js
@@ -0,0 +1,52 @@
+const model = [
+    {type: 'title', value: 'Hello world from JS'},
+    {type: 'text', value: 'some text...'},
+    {type: 'columns', value: [
+        '111111',
+        '222222',
+        '333333'
+    ]}
+]
+
+const $site = document.getElementById('site')
+
+model.forEach(block => {
+    let html = ''
+    if (block.type === 'title') {
+        html = `
+            <div class="row">
+                <div class="col-sm">
+                    <h1>${block.value}</h1>
+                </div>
+            </div>
+        `
+    }
+    else if (block.type === 'text') {
+        html = `
+            <div class="row">
+                <div class="col-sm">
+                    <p>${block.value}</p>
+                </div>
+            </div>
+        `
+    }
+    else if (block.type === 'columns'){
+        html = `
+            <div class="row">
+                <div class="col-sm">${block.value[0]}</div>
+                <div class="col-sm">${block.value[1]}</div>
+                <div class="col-sm">${block.value[2]}</div>
+            </div>
+        `
+    }
+
+
+    $site.insertAdjacentHTML('beforeend', html)
+
+})
+
+function title(block) {
+    return `
+        
+    `
+}
